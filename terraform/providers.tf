bf1b513dfa75b55a75f19ebcfa1c0b34f8a90b49
Yechen Qiao <yqiao@sapiosciences.com>
2025-09-16 17:44:35 -0400

    tf deps.

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -52,9 +52,9 @@ provider "kubernetes" {
 }
 # Helm provider pointed at the same cluster
 provider "helm" {
-  kubernetes {
-    host                   = module.eks.cluster_endpoint
+  kubernetes = {
+    host  = module.eks.cluster_endpoint
     cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-    token                  = data.aws_eks_cluster_auth.cluster_auth.token
+    token = data.aws_eks_cluster_auth.cluster_auth.token
   }
 }
